992bfd7db7dfc5ec3349728f729a80e9ce6bc1b9
Nicholas Tang <nicholas.yjtang@gmail.com>
2014-09-15 23:56:24 +0800

    Added better documentation and a slightly more intelligent way of downloading the files

--- get_data.R
+++ get_data.R
@@ -10,7 +10,7 @@ library(RCurl)
 get_data <- function(filename, url) { 
   #if the source file already exists, we can simply ignore downloading it again
   if (!file.exists(filename)) {
-    #file the directory name
+    #find the directory name
     directory_name <- dirname(filename)
     #if the directory does not exist, we create it
     if (!file.exists(directory_name)) {
@@ -27,22 +27,54 @@ get_data <- function(filename, url) {
 # @fileUrl the location of the file to download from 
 #========================================================
 download_file <- function (destination, fileUrl) {  
-  #set the directory name and filename first, along with the zip filename
+  #set the directory name and filename first
   directory_name <- dirname(destination)  #actual directory destination
-  filename <- basename(fileUrl)  #zip file name
-  zip_filename <- paste(directory_name,filename,sep="/")
-  
-  #check if the zip file already exists before downloading again
-  #if the file does not exist, then just download it again
-  if (!file.exists(zip_filename)) {
-    x <- getBinaryURL(fileUrl, ssl.verifypeer=FALSE)
-    to.write = file(zip_filename, "wb") 
-    writeBin(x,to.write)
-    close(to.write)
-  }  
+  filename <- basename(destination)  #destination filename without the directory path
 
-  #perform the unzip if the filename downloaded is a zip file
-  if (grep(".*\\.zip", filename)) {    
-    unzipped_files <- unzip(zip_filename, exdir = directory_name)    
+  #check if the file already exists before downloading again
+  #if the file does not exist, then just download it again
+  if (!file.exists(destination)) {
+    #using RCurl, we can skip the verification of the ssl
+    x <- getURLContent(fileUrl, ssl.verifypeer=FALSE, header=TRUE)
+    content_disposition = ""
+    #in some online downloads, the headers contain the name of the file under
+    #the http header "content-disposition" or "Content-disposition"
+    for (header in names(x$header)) {    
+      if (header == "Content-disposition" || header == "content-disposition") {
+        content_disposition <- x$header[header]
+      }
+    }
+    #in the case where content disposition exists
+    if (content_disposition != "") {
+      #we use regular expression to attempt to find the location and hence the substring
+      #for the name of the file
+      regular_expr_match <- "filename="
+      download_filename <- substr(content_disposition, gregexpr(regular_expr_match, content_disposition)[[1]][[1]]+nchar(regular_expr_match), nchar(content_disposition))    
+      end_index <- gregexpr(";", download_filename)[[1]][[1]] - 1
+      #some cases, the filename is the last item in the content-disposition
+      if (end_index == -2) {
+        end_index <- nchar(download_filename)
+      }
+      #set the filename to the substring
+      download_filename <- substr(download_filename, 1, end_index)
+      #remove the " if it is present in the filename
+      download_filename <- gsub("[\"]", "", download_filename)      
+    }
+    #the case where there is no content_disposition. we have to rely on the original filename given earlier
+    else {
+      download_filename = filename
+    }
+    #finally write the contents from memory/connection into the file
+    #close the file at the end
+    download_file_relativepath = paste(directory_name, download_filename, sep="/")
+    to.write = file(download_file_relativepath, "wb")    
+    writeBin(x$body[1:length(x$body)],to.write)
+    close(to.write)
+
+    #perform the unzip if the filename downloaded is a zip file
+    if (length(grep(".*\\.zip", download_filename)) != 0) {    
+      unzipped_files <- unzip(download_file_relativepath, exdir = directory_name)    
+    } 
   }  
 }
+
