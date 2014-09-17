1df2f181dbb955e80ae374f576a0ac57af77cbdd
Nicholas Tang <nicholas.yjtang@gmail.com>
2014-09-17 17:33:28 +0800

    Removed the method for using content disposition. stick to using something more simple, which is to basically download the file required to the destination

--- get_data.R
+++ get_data.R
@@ -1,10 +1,9 @@
 library(RCurl)
 #=============================================================
-#@author Nicholas Tang
-#the read data function
-#@filename the target file we wise to read and load. 
-#		Note that this will not perform any renaming operations
-#@url the url of the location of the original file
+# @author Nicholas Tang
+# the read data function
+# @filename the target file we wise to write to 
+# @url the url of the location of the original file
 #==============================================================
 
 get_data <- function(filename, url) { 
@@ -36,34 +35,10 @@ download_file <- function (destination, fileUrl) {
   if (!file.exists(destination)) {
     #using RCurl, we can skip the verification of the ssl
     x <- getURLContent(fileUrl, ssl.verifypeer=FALSE, header=TRUE)
-    content_disposition = ""
-    #in some online downloads, the headers contain the name of the file under
-    #the http header "content-disposition" or "Content-disposition"
-    for (header in names(x$header)) {    
-      if (header == "Content-disposition" || header == "content-disposition") {
-        content_disposition <- x$header[header]
-      }
-    }
-    #in the case where content disposition exists
-    if (content_disposition != "") {
-      #we use regular expression to attempt to find the location and hence the substring
-      #for the name of the file
-      regular_expr_match <- "filename="
-      download_filename <- substr(content_disposition, gregexpr(regular_expr_match, content_disposition)[[1]][[1]]+nchar(regular_expr_match), nchar(content_disposition))    
-      end_index <- gregexpr(";", download_filename)[[1]][[1]] - 1
-      #some cases, the filename is the last item in the content-disposition
-      if (end_index == -2) {
-        end_index <- nchar(download_filename)
-      }
-      #set the filename to the substring
-      download_filename <- substr(download_filename, 1, end_index)
-      #remove the " if it is present in the filename
-      download_filename <- gsub("[\"]", "", download_filename)      
-    }
-    #the case where there is no content_disposition. we have to rely on the original filename given earlier
-    else {
-      download_filename = filename
-    }
+
+
+     download_filename = filename
+
     #finally write the contents from memory/connection into the file
     #close the file at the end
     download_file_relativepath = paste(directory_name, download_filename, sep="/")
